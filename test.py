d9be044f18b6d65a5cb70e64d8523b5a525269b5
Mattywonger <twong50@jh.edu>
2023-09-09 16:32:47 -0400

    First commit

---
+++ test.py
@@ -0,0 +1,7 @@
+from pages import data_helper
+
+species_list=['Amphibians','Reptiles','Birds',"Mammals"]
+
+species=['Amphibians','Reptiles']
+
+print(data_helper.list_filtering(species,species_list))
